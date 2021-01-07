7201d20aba26d0742441bfa31c71ef512da11fa8
mrmisiex <misiek10061998@gmail.com>
2021-01-07 21:31:03 +0100

    Changed animation to home section, fixed styles

--- src/App.js
+++ src/App.js
@@ -1,153 +1,18 @@
 import "./App.scss";
-import React, { useRef, useEffect, useState } from "react";
+import React, { useRef } from "react";
 // import { useWindowScroll } from "react-use";
-import { ReactComponent as Slide } from "./First.svg";
-import { ReactComponent as Slide1400 } from "./First1400x800.svg";
-import { ReactComponent as Slide1024 } from "./First1024x600.svg";
-import { ReactComponent as Slide768 } from "./First768x600.svg";
-import { ReactComponent as Slide430 } from "./First430x630.svg";
-import { ReactComponent as Slide375 } from "./First375x500.svg";
-import { gsap } from "gsap";
+
 import Sections from "./Components/Sections";
 
 //gsap.registerPlugin(DrawSVGPlugin);
 
 function App() {
-  const wrapper = useRef(null);
   const refToTop = useRef(null);
-  const [isAnimationActive, setisAnimationActive] = useState(false);
-  const [isAnimationOn, setisAnimationOn] = useState(true);
 
   // const { y: pageYOffset } = useWindowScroll();
-  useEffect(() => {
-    const [elements] = wrapper.current.children;
-    const WelcomePage = elements.getElementById("WelcomePage");
-    const AboutMePage = elements.getElementById("AboutMePage");
-    const Page = elements.getElementById("Page");
-    gsap.set(WelcomePage, { autoAlpha: 0 });
-    gsap.set(Page, { transformOrigin: "50% 50%" });
-    gsap.set(AboutMePage, { y: -700 });
-
-    const tl = gsap.timeline({ defaults: { ease: "power3.inOut" } });
-    //const repeat = gsap.timeline({ repeat: 5 });
-    tl.fromTo(
-      WelcomePage,
-      { y: -500, scale: 1, transformOrigin: "50% 50%" },
-      { y: 0, duration: 3, autoAlpha: 1, ease: "bounce.out" }
-    );
-    const NextAnimation = () => {
-      setisAnimationActive(true);
-      console.log(isAnimationActive);
-    };
-    const SkipAnimation = () => {
-      setisAnimationOn(!isAnimationOn);
-    };
-
-    const AboutMeTexts = elements.getElementById("AboutMeTexts");
-    const OldPageWider = elements.getElementById("OldPageWider");
-    const AboutMeLinks = elements.getElementById("AboutMeLinks");
-    const AboutPage = elements.getElementById("AboutPage");
-    const HomePage = elements.getElementById("HomePage");
-    const ProjectsPage = elements.getElementById("ProjectsPage");
-    const SkillsPage = elements.getElementById("SkillsPage");
-    const ContactPage = elements.getElementById("ContactPage");
-    const Skip = elements.getElementById("SkipAnimation");
-
-    AboutPage.addEventListener("click", () => {
-      window.scrollTo({
-        top: document.querySelector(".about").offsetTop,
-        behavior: "smooth",
-      });
-      setisAnimationOn(!isAnimationOn);
-    });
-
-    ProjectsPage.addEventListener("click", () => {
-      window.scrollTo({
-        top: document.querySelector(".projects").offsetTop,
-        behavior: "smooth",
-      });
-      setisAnimationOn(!isAnimationOn);
-    });
-
-    HomePage.addEventListener("click", () => {
-      window.scrollTo({
-        top: document.querySelector(".home").offsetTop,
-        behavior: "smooth",
-      });
-      setisAnimationOn(!isAnimationOn);
-    });
-
-    SkillsPage.addEventListener("click", () => {
-      window.scrollTo({
-        top: document.querySelector(".skills").offsetTop,
-        behavior: "smooth",
-      });
-      setisAnimationOn(!isAnimationOn);
-    });
-
-    ContactPage.addEventListener("click", () => {
-      window.scrollTo({
-        top: document.querySelector(".contact").offsetTop,
-        behavior: "smooth",
-      });
-      setisAnimationOn(!isAnimationOn);
-    });
-
-    document.getElementById("Page").addEventListener("click", NextAnimation);
-    Skip.addEventListener("click", SkipAnimation);
-
-    gsap.set([...AboutMeTexts.children, ...AboutMeLinks.children], {
-      autoAlpha: 0,
-    });
-    gsap.set(OldPageWider, { autoAlpha: 1 });
-
-    const tl2 = gsap.timeline({ defaults: { ease: "power3.inOut" } });
-
-    if (isAnimationActive === true) {
-      tl2
-        .to(WelcomePage, {
-          y: 900,
-          duration: 2.5,
-          ease: "power3.in",
-          scale: 0.3,
-        })
-        .to(AboutMePage, {
-          y: 0,
-          duration: 4,
-          ease: "bounce.out",
-          delay: -2,
-        })
-        .to(AboutMeTexts.children, {
-          duration: 3,
-          autoAlpha: 1,
-          stagger: 1.4,
-          delay: -3,
-        })
-        .to(AboutMeLinks.children, { duration: 3, autoAlpha: 1, stagger: 0.4 });
-    } else {
-    }
-  }, [isAnimationActive, isAnimationOn]);
 
   return (
     <div ref={refToTop} className="App">
-      {isAnimationOn ? (
-        <div className="welcomeAnimation" ref={wrapper}>
-          {window.innerWidth < 375 ? (
-            <Slide375 />
-          ) : window.innerWidth < 430 ? (
-            <Slide430 />
-          ) : window.innerWidth < 768 ? (
-            <Slide768 />
-          ) : window.innerWidth < 1024 ? (
-            <Slide1024 />
-          ) : window.innerWidth < 1400 ? (
-            <Slide1400 />
-          ) : (
-            <Slide />
-          )}
-          {/* <Slide /> */}
-        </div>
-      ) : null}
       <Sections />
     </div>
   );
--- src/Components/Sections/Home.js
+++ src/Components/Sections/Home.js
@@ -1,10 +1,91 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Nav from "./Nav";
+import { ReactComponent as HomeAnimation1920 } from "../../HomeAnimation1920x980.svg";
+import { ReactComponent as HomeAnimation1440 } from "../../HomeAnimation1440x800.svg";
+import { ReactComponent as HomeAnimation1024 } from "../../HomeAnimation1024x600.svg";
+import { ReactComponent as HomeAnimation768 } from "../../HomeAnimation768x600.svg";
+import { ReactComponent as HomeAnimation420 } from "../../HomeAnimation420x600.svg";
+import { ReactComponent as HomeAnimation320 } from "../../HomeAnimation320x500.svg";
+import { gsap } from "gsap";
 
 function Home() {
+  const wrapper2 = useRef(null);
+
+  useEffect(() => {
+    const [elements] = wrapper2.current.children;
+    const Steps1 = elements.getElementById("Steps");
+    // const Steps2 = elements.getElementById("Steps2");
+    // const Steps3 = elements.getElementById("Steps3");
+    // const Steps4 = elements.getElementById("Steps4");
+
+    const duration = 1;
+    const stagger = 0.4;
+
+    gsap.set([...Steps1.children], { autoAlpha: 0 });
+    const tl = gsap.timeline({
+      defaults: { ease: "Power1.easeIn" },
+    });
+
+    if (window.innerWidth < 426) {
+      tl.to(Steps1.children, {
+        duration: duration,
+        autoAlpha: 1,
+        stagger: stagger,
+        delay: 0,
+      })
+        .to(Steps1.children, {
+          duration: duration,
+          autoAlpha: 0,
+          stagger: stagger,
+          delay: -30,
+        })
+        .repeat(-1);
+    } else {
+      tl.to(Steps1.children, {
+        duration: duration,
+        autoAlpha: 1,
+        stagger: stagger,
+        delay: 0,
+      })
+        .to(Steps1.children, {
+          duration: duration,
+          autoAlpha: 0,
+          stagger: stagger,
+          delay: -45,
+        })
+        .repeat(-1);
+    }
+  }, [window.innerWidth]);
+
   return (
     <div className="home">
       <Nav />
+      <div className="home_animation" ref={wrapper2}>
+        {window.innerWidth < 375 ? (
+          <HomeAnimation320 />
+        ) : window.innerWidth < 430 ? (
+          <HomeAnimation420 />
+        ) : window.innerWidth < 768 ? (
+          <HomeAnimation768 />
+        ) : window.innerWidth < 1024 ? (
+          <HomeAnimation1024 />
+        ) : window.innerWidth < 1400 ? (
+          <HomeAnimation1440 />
+        ) : (
+          <HomeAnimation1920 />
+        )}
+      </div>
+      <div className="home_name">
+        Michał Dulko
+        <br />
+        Frontend
+        <br />
+        Developer
+        <br />
+        Design
+        <br />
+        Websites
+      </div>
     </div>
   );
 }
